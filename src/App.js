715fa556476bb3cdf7c1284c5ca86caf9763d2f8
HS940405 <hansol.yang.94@gmail.com>
2023-01-13 20:12:55 -0800

    Revised App.js & App.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import top1 from './top1.png';
+import top2 from './top2.png';
+import top3 from './top3.png';
+import bottom1 from './bottom1.png';
+import bottom2 from './bottom2.png';
+import bottom3 from './bottom3.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={top1} className="App-top1" alt="top1" />
+        <img src={top2} className="App-top2" alt="top2" />
+        <img src={top3} className="App-top3" alt="top3" />
+        <img src={bottom1} className="App-bottom1" alt="bottom1" />
+        <img src={bottom2} className="App-bottom2" alt="bottom2" />
+        <img src={bottom3} className="App-bottom3" alt="bottom3" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          My Clothes
       </header>
     </div>
   );
