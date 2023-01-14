2b4e5699eae30402d13f8fcc88bdfe0c8a7f90dc
HS940405 <hansol.yang.94@gmail.com>
2023-01-14 09:42:32 -0800

    Dividing slider into top and bottom

---
+++ src/data_bottom.js
@@ -0,0 +1,39 @@
+export const data_bottom = [
+    // {
+    //   id: 1,
+    //   img: 'https://aritzia.scene7.com/is/image/Aritzia/hi-res/s23_04_a04_101467_11420_off_a.jpg',
+    // },
+    {
+      id: 2,
+      img: 'https://aritzia.scene7.com/is/image/Aritzia/hi-res/s23_01_a06_81775_27638_off_b.jpg',
+    },
+    // {
+    //   id: 3,
+    //   img: 'https://aritzia.scene7.com/is/image/Aritzia/f22_00_a01_104277_1274_off_b?wid=1800',
+    // },
+    // {
+    //   id: 4,
+    //   img: 'https://aritzia.scene7.com/is/image/Aritzia/f22_01_a01_81266_27359_off_a?wid=900',
+    // },
+    // {
+    //   id: 5,
+    //   img: 'https://aritzia.scene7.com/is/image/Aritzia/s22_07_a01_77656_1275_off_a?wid=900',
+    // },
+    {
+      id: 6,
+      img: 'https://aritzia.scene7.com/is/image/Aritzia/f22_07_a06_79143_19862_off_a?wid=1800',
+    },
+    // {
+    //   id: 7,
+    //   img: 'https://aritzia.scene7.com/is/image/Aritzia/f22_02_a08_100975_1702_off_a?wid=900',
+    // },
+    // {
+    //   id: 8,
+    //   img: 'https://aritzia.scene7.com/is/image/Aritzia/f22_01_a01_91282_4322_off_a?wid=900',
+    // },
+    {
+      id: 9,
+      img: 'https://aritzia.scene7.com/is/image/Aritzia/s22_01_a06_102335_1274_off_a?wid=1800',
+    }
+    
+  ];
