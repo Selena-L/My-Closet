c803896a1d108cc9a4ebabe8606a894674e3d93b
Kristen Li <kristen.yc.li@gmail.com>
2023-01-14 01:28:48 -0800

    committry1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,73 @@
-import top1 from './top1.png';
-import top2 from './top2.png';
-import top3 from './top3.png';
-import bottom1 from './bottom1.png';
-import bottom2 from './bottom2.png';
-import bottom3 from './bottom3.png';
 import {Accessible} from './Accessible';
 
-import './App.css';
-
+import React from 'react';
+import { data } from './mockData';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 function App() {
+  const slideLeft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={top1} className="App-top1" alt="top1" />
-        <img src={top2} className="App-top2" alt="top2" />
-        <img src={top3} className="App-top3" alt="top3" />
-        <img src={bottom1} className="App-bottom1" alt="bottom1" />
-        <img src={bottom2} className="App-bottom2" alt="bottom2" />
-        <img src={bottom3} className="App-bottom3" alt="bottom3" />
-        <p>
-        My Clothes
-        </p>
-          
-      </header>
-      <section className="section">
+    <><>
+      <img
+        className='w-full h-[440px] object-cover'
+        src='https://download.logo.wine/logo/Aritzia/Aritzia-Logo.wine.png'
+        alt='' />
+      <h1>My Closet</h1>
+      <div className='relative flex items-center'>
+        <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} size={40} />
+        <div
+          id='slider'
+          className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'
+        >
+          {data.map((item) => (
+            <img
+              className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'
+              src={item.img}
+              alt='/' />
+          ))}
+        </div>
+        <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideRight} size={40} />
+      </div>
+
+    </><section className="section">
         <h2>Recommendation</h2>
         <Accessible />
-    </section>
-    </div>
-  );
+      </section></>
+  )
 }
 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={top1} className="App-top1" alt="top1" />
+//         <img src={top2} className="App-top2" alt="top2" />
+//         <img src={top3} className="App-top3" alt="top3" />
+//         <img src={bottom1} className="App-bottom1" alt="bottom1" />
+//         <img src={bottom2} className="App-bottom2" alt="bottom2" />
+//         <img src={bottom3} className="App-bottom3" alt="bottom3" /> */}
+//         <Scroll />
+//         <p>
+//         My Clothes
+//         </p>
+          
+//       </header>
+//       <section className="section">
+//         <h2>Recommendation</h2>
+//         <Accessible />
+//     </section>
+//     </div>
+//   );
+// }
+
+// export default App;
